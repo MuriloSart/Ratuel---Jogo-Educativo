af0ab2801ebd6ed7e5d802a38eed293aa01986d6
MuriloSart <muriloasartori@gmail.com>
2025-04-06 08:11:51 -0300

    Feat SFX

--- Assets/Script/Painel/ConteinerLetterHandler.cs
+++ Assets/Script/Painel/ConteinerLetterHandler.cs
@@ -2,12 +2,26 @@ using UnityEngine;
 
 public class ConteinerLetterHandler : MonoBehaviour
 {
+    public AudioSource hitSound;
+    public AudioSource missSound;
+
     public void ConferLetters()
     {
+        bool allCorrect = true;
+
         for (int i = 0; i < transform.childCount; i++)
         {
-            Transform child = transform.GetChild(i);
-            if (!child.GetComponent<ComparisonGrid>().IsCorrect) break;
+            var child = transform.GetChild(i);
+            var comparison = child.GetComponent<ComparisonGrid>();
+
+            if (!comparison.IsCorrect)
+            {
+                allCorrect = false;
+                break;
+            }
         }
+
+        if (allCorrect)
+            hitSound.Play();
     }
 }
